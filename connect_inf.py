8872b641dcc22a6427f0402d494528693880f52e
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-01-07 18:10:14 +1300

    connect_inf.py file created and rules established

---
+++ connect_inf.py
@@ -0,0 +1,10 @@
+"""
+Connect Infinity
+
+Rules:
+    - First connect 4 is played as normally
+    - After a winner of connect 4 is decided, connect 5 will begin on the same board (previous counters will be left on the board and are still active)
+    - When playing connect 5 each player will get 2 counters per turn
+    - After the games finished then connect 6 will be played (same board and previous counters from connect 4 and 5 are active)
+    - The player who has won the most rounds of connect game win. 
+"""
