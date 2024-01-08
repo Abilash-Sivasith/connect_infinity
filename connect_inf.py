77fe5a892daa7030a49782fb9238c4d71dc8874f
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-01-08 15:13:38 +1300

    created consts and create_board and drop_peice function

--- connect_inf.py
+++ connect_inf.py
@@ -8,3 +8,25 @@ Rules:
     - After the games finished then connect 6 will be played (same board and previous counters from connect 4 and 5 are active)
     - The player who has won the most rounds of connect game win. 
 """
+
+import numpy as np
+import pygame
+import math
+
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255 ,0 ,0)
+YELLOW = (255, 255, 0)
+
+ROW_COUNT = 10
+COL_COUNT = 10
+
+def create_board():
+    '''creates the game board'''
+    board = np.zeros((ROW_COUNT, COL_COUNT))
+    return board
+
+def drop_peice(board, row, col, peice):
+    """sets the row and column in which the peice has been dropped"""
+    board[row][col] = peice
+
