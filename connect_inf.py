c6944b807aad71ab6887cdca8f0ef2a6525ebc67
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-01-09 13:01:37 +1300

    increased readability of function

--- connect_inf.py
+++ connect_inf.py
@@ -65,6 +65,7 @@ def winning_move_connect_3(board, peice):
         for row in range(3, ROW_COUNT):
             if board[row][col] == peice and board[row - 1][col + 1] == peice and board[row - 3][col + 3] == peice:
                 return True # winning move by 3 in a row diagonal slope downwards
+            
     for col in range(COL_COUNT - 3):
         for row in range(ROW_COUNT - 3):
             if board[row][col] == peice and board[row + 1][col + 1] == peice and board[row + 2][col + 2] == peice:
